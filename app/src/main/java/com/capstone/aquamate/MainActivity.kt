5d545b3e9fc349013772ba7a9ab4f1f83bb8f7d9
unknown <dewifatimahazzahra4@gmail.com>
2024-06-19 12:45:03 +0700

    Update terbaru

--- app/src/main/java/com/capstone/aquamate/MainActivity.kt
+++ app/src/main/java/com/capstone/aquamate/MainActivity.kt
@@ -3,10 +3,9 @@ package com.capstone.aquamate
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,10 +15,9 @@ import com.capstone.aquamate.databinding.ActivityMainBinding
 import com.capstone.aquamate.factory.MainViewModelFactory
 import com.capstone.aquamate.viewmodel.MainViewModel
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var rvRecomendation : RecyclerView
+    private lateinit var rvRecomendation: RecyclerView
     private lateinit var recomendationAdapter: RecomendationAdapter
     private val list = ArrayList<recomendation>()
     private val mainViewModel: MainViewModel by viewModels { MainViewModelFactory(application) }
@@ -29,7 +27,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        rvRecomendation = findViewById(R.id.rv_recomndation)
+        // Setting up the RecyclerView
+        rvRecomendation = binding.rvRecomndation
         rvRecomendation.setHasFixedSize(true)
 
         list.addAll(getListRecomendation())
@@ -46,7 +45,6 @@ class MainActivity : AppCompatActivity() {
         binding.fishPrediction.setImageBitmap(resizeBitmap(R.drawable.fish_scanning, 80, 80))
         binding.fishDictionary.setImageBitmap(resizeBitmap(R.drawable.fish_dictionary, 80, 80))
         binding.socialFish.setImageBitmap(resizeBitmap(R.drawable.social_fish, 80, 80))
-       // binding.rvRecomndation.setImageBitmap(resizeBitmap(R.drawable.gurame, 100, 100))
 
         // Set onClickListeners
         binding.fishPrediction.setOnClickListener {
@@ -62,6 +60,11 @@ class MainActivity : AppCompatActivity() {
         binding.keluar.setOnClickListener {
             mainViewModel.logout()
             navigateToLogin()
+        }
+
+        // Set onClickListener untuk FishMaps (ImageView)
+        binding.FishMaps.setOnClickListener {
+            openMaps()
         }
     }
 
@@ -94,7 +97,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Function to calculate the inSampleSize value
-    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+    private fun calculateInSampleSize(
+        options: BitmapFactory.Options,
+        reqWidth: Int,
+        reqHeight: Int
+    ): Int {
         val height = options.outHeight
         val width = options.outWidth
         var inSampleSize = 1
@@ -107,13 +114,15 @@ class MainActivity : AppCompatActivity() {
         }
         return inSampleSize
     }
+
     private fun getListRecomendation(): ArrayList<recomendation> {
         val dataName = resources.getStringArray(R.array.data_name)
         val dataDescription = resources.getStringArray(R.array.data_description)
         val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
         val listRecomendation = ArrayList<recomendation>()
         for (i in dataName.indices) {
-            val recomendation = recomendation(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            val recomendation =
+                recomendation(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
             listRecomendation.add(recomendation)
         }
         return listRecomendation
@@ -123,17 +132,29 @@ class MainActivity : AppCompatActivity() {
         rvRecomendation.layoutManager = LinearLayoutManager(this)
         recomendationAdapter = RecomendationAdapter(list)
         rvRecomendation.adapter = recomendationAdapter
-        recomendationAdapter.setOnItemClickCallback(object : RecomendationAdapter.OnItemClickCallback {
+        recomendationAdapter.setOnItemClickCallback(object :
+            RecomendationAdapter.OnItemClickCallback {
             override fun onItemClicked(data: recomendation) {
-                showSelectedFilm(data)
+                showSelectedRecomendation(data)
             }
         })
     }
 
-    private fun showSelectedFilm(recomendation: recomendation) {
+    private fun showSelectedRecomendation(recomendation: recomendation) {
         val intentDetail = Intent(this, DetailRecomendationActivity::class.java)
-        intentDetail.putExtra("key_film", recomendation)
+        intentDetail.putExtra("key_recomendation", recomendation)
         startActivity(intentDetail)
     }
-}
 
+    private fun openMaps() {
+        // Membuat query untuk pencarian tempat penjualan ikan terdekat
+        val gmmIntentUri = Uri.parse("geo:0,0?q=penjualan ikan terdekat")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+
+        // Pastikan Google Maps tersedia
+        if (mapIntent.resolveActivity(packageManager) != null) {
+            startActivity(mapIntent)
+        }
+    }
+}
